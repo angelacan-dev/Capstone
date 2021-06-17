28034f9afa4f62f52c9739fb998f296eb0730fc1
Angela Smith <angelacsmith123@gmail.com>
2021-06-17 17:52:31 -0500

    Assignment 5 part 2

---
+++ Bank/Bank/src/main/java/com/meritamericabank/bank/controller/AccountHolderController.java
@@ -0,0 +1,114 @@
+package com.meritamericabank.bank.controller;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.meritamericabank.bank.models.AccountHolder;
+import com.meritamericabank.bank.models.CDAccount;
+import com.meritamericabank.bank.models.CheckingAccount;
+import com.meritamericabank.bank.models.SavingsAccount;
+
+@RestController
+public class AccountHolderController {
+
+	Map<Integer, AccountHolder> holders = new HashMap<>();
+	AtomicInteger nextAccountId = new AtomicInteger(0);
+
+	@PostMapping("/AccountHolders")
+	public ResponseEntity<AccountHolder> create(@Valid @RequestBody AccountHolder holder) {
+		int id = nextAccountId.incrementAndGet();
+		holder.setAccountId(id);
+		holders.put(id, holder);
+		return new ResponseEntity<>(holder, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/AccountHolders")
+	public ResponseEntity getAll() {
+		Collection<AccountHolder> list = holders.values();
+		return new ResponseEntity(list, HttpStatus.OK);
+//		CDOffering[] list = new CDOffering[offerings.size()];
+	}
+
+
+
+	@GetMapping("/AccountHolders/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public AccountHolder getAccountHolder(@PathVariable int id) {
+		return holders.get(id);
+	}
+
+	@PostMapping("/AccountHolders/{id}/CheckingAccounts")
+	public CheckingAccount addCheckingAccount(@PathVariable int id, @RequestBody @Valid CheckingAccount checkingAccount) {
+		AccountHolder holder = holders.get(id);
+		holder.addCheckingAccount(checkingAccount);
+		
+		return checkingAccount;
+	}
+
+	@GetMapping("/AccountHolders/{id}/CheckingAccounts")
+	public CheckingAccount[] getCheckingAccount(@PathVariable int id) {
+		return holders.get(id).getCheckingAccounts();
+	}
+
+	@PostMapping("/AccountHolders/{id}/SavingsAccounts")
+	public SavingsAccount addSavingsAccount(@PathVariable int id, @RequestBody @Valid SavingsAccount sa) {
+		AccountHolder holder = holders.get(id);
+		holder.addSavingsAccount(sa);
+		return sa;
+	}
+
+	@GetMapping("/AccountHolders/{id}/SavingsAccounts")
+	public SavingsAccount[] getSavingsAccount(@PathVariable int id) {
+		return holders.get(id).getSavingsAccounts();
+		
+	}
+
+	
+	@PostMapping("/AccountHolders/{id}/CDAccounts")
+	public CDAccount addCDAccount(@PathVariable int id, @RequestBody @Valid CDAccount cdAccount) {
+		holders.get(id).addCDAccount(cdAccount);
+		return cdAccount;
+	}
+
+	@GetMapping("/AccountHolders/{id}/CDAccounts")
+	public ResponseEntity<CDAccount[]> getCDAccount(@PathVariable int id) {
+		
+		if (holders.containsKey(id)) {
+			CDAccount[] accounts =  holders.get(id).getCDAccounts();
+			return new ResponseEntity<>(accounts, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
+	}
+
+}
---
+++ Bank/Bank/src/main/java/com/meritamericabank/bank/models/CDOffering.java
@@ -0,0 +1,57 @@
+package com.meritamericabank.bank.models;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Range;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class CDOffering {
+	private int id;
+	//@NotBlank(message = "Term is mandatory")
+	@Range(min = 1, message = "Term cannot be less than 1" )
+	private int term;
+	//@NotBlank(message = "Interest rate is mandatory")
+	@Min(value = 0, message = "Interest rate must be between 0 and 1")
+	@Max(value = 1, message = "Interest rate must be between 0 and 1")
+	private double interestRate;
+	
+	public CDOffering() {
+		
+	}
+
+	public CDOffering(int term, double interestRate) {
+		this.term = term;
+		this.interestRate = interestRate;
+	}
+	
+	public int getTerm() {
+		return term;
+	}
+	public void setTerm(int term) {
+		this.term = term;
+	}
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+}
